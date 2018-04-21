17d415119dfa080c005ed2ebb0c7474e9b0375a4
David Danko <dcdanko@gmail.com>
2018-04-21 15:11:55 +0200

    script to make tables

--- capalyzer/cli.py
+++ capalyzer/cli.py
@@ -24,27 +24,35 @@ def tables(dirname, tables):
 
     dff = DataTableFactory(dirname)
 
+    print('Making taxonomy tables...')
     dff.taxonomy.kraken(top_n=100).to_csv(tables + '/kraken_species_top100.csv')
     dff.taxonomy.kraken(top_n=0, cutoff=0.01).to_csv(tables + '/kraken_species_1perc_cutoff.tsv')
 
+    print('Making AMR tables...')
     dff.amr.mech().to_csv(tables + '/amr_mech.csv')
     dff.amr.gene().to_csv(tables + '/amr_gene.csv')
     dff.amr.classus().to_csv(tables + '/amr_class.csv')
     dff.amr.group().to_csv(tables + '/amr_group.csv')
 
+    print('Making ags tables...')
     dff.ags.tbl().to_csv(tables + '/ags.csv')
 
+    print('Making alpha diversity tables...')
     dff.alpha.shannon()['100000'].to_csv(tables + '/alpha_diversity_shannon.csv')
     dff.alpha.chao1()['100000'].to_csv(tables + '/alpha_diversity_chao1.csv')
     dff.alpha.richness()['100000'].to_csv(tables + '/alpha_diversity_richness.csv')
 
+    print('Making pathway tables...')
     dff.pathways.pathways().to_csv(tables + '/pathways.csv')
 
+    print('Making virulence tables...')
     dff.vir.rpkm().to_csv(tables + '/virulence_factors_rpkm.csv')
     dff.vir.rpkmg().to_csv(tables + '/virulence_factors_rpkmg.csv')
 
+    print('Making methyltransferase tables...')
     dff.methyls.rpkm().to_csv(tables + '/methyltransferases_rpkm.csv')
     dff.methyls.rpkmg().to_csv(tables + '/methyltransferases_rpkmg.csv')
 
+    print('Making HMP tables...')
     write_json(dff.hmp.raw(), tables + '/hmp_raw.json')
     write_json(dff.hmp.dists(), tables + '/hmp_dists.json')
