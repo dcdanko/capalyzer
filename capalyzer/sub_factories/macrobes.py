b5ff8cc78b2f3f181f08fa775af20c6dd418ac41
David Danko <dcdanko@gmail.com>
2018-05-01 22:58:51 -0400

    code

---
+++ capalyzer/sub_factories/macrobes.py
@@ -0,0 +1,23 @@
+from .subfactory import SubFactory
+from pandas import DataFrame
+from json import loads
+from numpy import percentile
+
+
+def jloads(fname):
+    return loads(open(fname).read())
+
+
+class MacrobeFactory(SubFactory):
+
+    def table(self):
+        macrobefs = self.factory.get_results(
+            module='quantify_macrobial',
+            result='tbl'
+        )
+        tbl = DataFrame.from_dict({
+            sname: {
+                taxa: vals['rpkm'] for taxa, vals in jloads(fname).items()
+            } for sname, fname
+        }, orient='index')
+        return tbl
