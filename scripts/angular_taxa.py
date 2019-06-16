07049728f7f9a4951d678f49a764372548b8d4a5
David Danko <dcdanko@gmail.com>
2019-06-16 18:12:09 -0400

    Modified anguler (ratio) taxa parser

--- scripts/angular_taxa.py
+++ scripts/angular_taxa.py
@@ -8,22 +8,20 @@ from math import log10
 @click.option('-k', '--min-kmers', default=64, type=int)
 @click.option('-r', '--min-reads', default=3, type=int)
 @click.option('-s', '--slope', default=(100 / 250), type=float)
+@click.option('--rank', default='species')
 @click.argument('longform_table', type=click.File('r'))
 @click.argument('table_out', type=click.File('w'))
-def main(min_kmers, min_reads, slope, longform_table, table_out):
+def main(min_kmers, min_reads, slope, rank, longform_table, table_out):
     """Filter taxa according to the ratio of reads to kmers."""
     long_taxa = pd.read_csv(longform_table)
-    long_taxa.columns = [
-        'uuid', 'taxa', 'taxid', 'rank', 'mpa',
-        'cov', 'dup', 'kmers', 'percent', 'reads', 'tax_reads'
-    ]
-    long_taxa = long_taxa.query('rank == "species"')
+    long_taxa.columns = ['uuid', 'taxa', 'taxid', 'rank'] + long_taxa.columns.tolist()[4:]
+    long_taxa = long_taxa.query('rank == @rank')
     long_taxa = long_taxa.query('reads >= @min_reads')
     long_taxa = long_taxa.query('kmers >= @min_kmers')
     
     def below_slope(row):
-        my_slope = log10(row['reads']) / log10(row['kmers'])
-        return my_slope <= slope
+        my_slope = int(row['reads']) / int(row['kmers'])
+        return my_slope <= slope or row['cov'] >= 0.9
 
     rows_below = long_taxa.apply(lambda r: below_slope(r), axis=1)
     long_taxa = long_taxa.loc[rows_below]
